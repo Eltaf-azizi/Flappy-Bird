245b172f9e74247506f7ebda56fa93d5bd51b0af
Eltaf-azizi <iltfhuss@gmail.com>
2024-12-29 20:48:12 +0500

    added the background

--- FlappyBird.java
+++ FlappyBird.java
@@ -1,17 +1,44 @@
 import java.awt.*;
-import java.awt.event.*;
-import java.util.ArrayList;
-import java.util.Random;
 import javax.swing.*;
 
-public class FlappyBird{
+public class FlappyBird extends JPanel {
     int boardWidth = 360;
     int boardHeight = 640;
 
+    // Images
+    Image backgroundImg;
+    Image birdImg;
+    Image topPipeImg;
+    Image bottomPipeImage;
 
     FlappyBird() {
         setPreferredSize(new Dimension(boardWidth, boardHeight));
-        setBackground(Color.blue);
+
+        // Load images
+        backgroundImg = new ImageIcon(getClass().getResource("./flappybirdng.png")).getImage();
+        birdImg = new ImageIcon(getClass().getResource("./flappybird.png")).getImage();
+        topPipeImg = new ImageIcon(getClass().getResource("./toppipe.png")).getImage();
+        bottomPipeImage = new ImageIcon(getClass().getResource("./bottompipeimage.png")).getImage();
     }
 
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g); // Call the JPanel's paintComponent method
+        draw(g);
+    }
+
+    private void draw(Graphics g) {
+        g.drawImage(backgroundImg, 0, 0, boardWidth, boardHeight, null);
+        // Add more drawing logic as needed
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Flappy Bird");
+        FlappyBird gamePanel = new FlappyBird();
+
+        frame.add(gamePanel);
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
 }
