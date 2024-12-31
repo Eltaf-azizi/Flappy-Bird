74e9bfd7cdf99365ae77ab2c0ab74255d275d655
Eltaf-azizi <iltfhuss@gmail.com>
2024-12-31 20:53:18 +0500

    working on the functionality of bird

--- FlappyBird.java
+++ FlappyBird.java
@@ -1,7 +1,9 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 
-public class FlappyBird extends JPanel{
+public class FlappyBird extends JPanel implements ActionListener {
     int boardWidth = 360;
     int boardHeight = 640;
 
@@ -12,12 +14,17 @@ public class FlappyBird extends JPanel{
     Image bottomPipeImage;
 
     //Bird
-    int birdX = boardWidth/8;
-    int birdY = boardHeight/2;
+    int birdX = boardWidth / 8;
+    int birdY = boardHeight / 2;
     int birdWidth = 34;
     int birdHeight = 24;
 
-    class Bird{
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        repaint();
+    }
+
+    class Bird {
         int x = birdX;
         int y = birdY;
         int width = birdWidth;
@@ -29,34 +36,54 @@ public class FlappyBird extends JPanel{
         }
     }
 
-    // game logic
+    // Game logic
     Bird bird;
+    int velocityY = 0;
+
+
+    Timer gameloop;
 
     FlappyBird() {
         setPreferredSize(new Dimension(boardWidth, boardHeight));
 
         // Load images
-        backgroundImg = new ImageIcon(getClass().getResource("./flappybirdng.png")).getImage();
-        birdImg = new ImageIcon(getClass().getResource("./flappybird.png")).getImage();
-        topPipeImg = new ImageIcon(getClass().getResource("./toppipe.png")).getImage();
-        bottomPipeImage = new ImageIcon(getClass().getResource("./bottompipeimage.png")).getImage();
+        backgroundImg = new ImageIcon("flappybirdng.png").getImage();
+        birdImg = new ImageIcon("flappybird.png").getImage();
+        topPipeImg = new ImageIcon("toppipe.png").getImage();
+        bottomPipeImage = new ImageIcon("bottompipeimage.png").getImage();
 
-        //bird
+        // Bird
         bird = new Bird(birdImg);
+
+        // Game timer
+        gameloop = new Timer(1000 / 60, this); // 1000/60 = 16.6
+        gameloop.start();
     }
 
-    
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         draw(g);
     }
 
     private void draw(Graphics g) {
+        System.out.println("draw");
+
         g.drawImage(backgroundImg, 0, 0, boardWidth, boardHeight, null);
 
         // Bird
         g.drawImage(bird.img, bird.x, bird.y, bird.width, bird.height, null);
-        
     }
 
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Flappy Bird");
+        FlappyBird flappyBird = new FlappyBird();
+
+        frame.add(flappyBird);
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+        flappyBird.gameloop.start();
+    }
 }
